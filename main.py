46ebfb85b77ffe69a6b74494647870e13de478f7
ldc902 <ldc902tpqt@gmail.com>
2026-06-25 02:38:17 +0700

    commit

--- main.py
+++ main.py
@@ -5,50 +5,70 @@ import pandas as pd
 
 model = load_model("./model/model.keras")
 
-with open('./model/X_scaler.pkl', 'rb') as f1:
-   X_scaler = pickle.load(f1)
-   
-with open('./model/Y_scaler.pkl', 'rb') as f2:
-   Y_scaler = pickle.load(f2)
+with open('./model/ocp_onehot_encoder.pkl', 'rb') as f1:
+    ocp_onehot_encoder = pickle.load(f1)
 
-with open('./model/ocp_encoder.pkl', 'rb') as f3:
-    ocp_encoder = pickle.load(f3)
+with open('./model/X_standard_scaler.pkl', 'rb') as f2:
+   X_standard_scaler = pickle.load(f2)
+   
+with open('./model/X_robust_scaler.pkl', 'rb') as f3:
+   X_robust_scaler = pickle.load(f3)
+   
+with open('./model/Y_standard_scaler.pkl', 'rb') as f4:
+   Y_standard_scaler = pickle.load(f4)
+   
+with open('./model/data_preprocessor.pkl', 'rb') as f5:
+   data_preprocessor = pickle.load(f5)
+
 
 def prepare_input(input): 
-    col_names = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'ocean_proximity']
+    X_col_names = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'ocean_proximity']
        
     if not isinstance(input, pd.DataFrame):
         X_predict = pd.DataFrame(
             input,
-            columns = col_names,
+            columns = X_col_names,
         )
+        
     X_predict["ocean_proximity"] = X_predict["ocean_proximity"].str.strip().str.lower()
 
-    ocp_encoded = ocp_encoder.transform(X_predict[["ocean_proximity"]]) # (-1, 5)
+    X_ocp = ocp_onehot_encoder.transform(X_predict[["ocean_proximity"]]) # (-1, 5)
     
-    ocp_encoded = pd.DataFrame(
-        ocp_encoded,
-        columns = ocp_encoder.get_feature_names_out(
+    X_ocp = pd.DataFrame(
+        X_ocp,
+        columns = ocp_onehot_encoder.get_feature_names_out(
             ['ocean_proximity']
         ),
         index = X_predict.index
     )
-
-    X_remain = X_scaler.transform(X_predict.drop(columns=["ocean_proximity"])) # (-1, 8)
     
-    X_remain = pd.DataFrame(
-        X_remain,
-        columns = X_predict.drop(columns=["ocean_proximity"]).columns,
-        index = X_predict.index
+    normal_col_names = [
+        "longitude",
+        "latitude",
+        "housing_median_age",
+        "median_income"
+    ]
+    
+    skew_col_names = [
+        "total_rooms",
+        "total_bedrooms",
+        "population",
+        "households"
+    ]
+    
+    X_cols_rest = pd.DataFrame(
+        data_preprocessor.transform(X_predict.drop(columns=["ocean_proximity"])),  # transform
+        columns=X_predict.drop(columns=["ocean_proximity"]).columns,
+        index=X_predict.index
     )
-        
-    X_predict = pd.concat([X_remain, ocp_encoded], axis=1) # (-1, 13)
+     
+    X_predict = pd.concat([X_cols_rest, X_ocp], axis=1) # (-1, 13)
        
     return X_predict
 
 def predict(X_predict):
     Y_predict = model.predict(X_predict)
-    return Y_scaler.inverse_transform(Y_predict)
+    return Y_standard_scaler.inverse_transform(Y_predict)
 
 input = [
     {
@@ -90,5 +110,7 @@ input = [
 ]
 
 X_predict = prepare_input(input)
+
 Y_predict = predict(X_predict)
+
 print(f"Predicted median_house_value: {Y_predict}")
